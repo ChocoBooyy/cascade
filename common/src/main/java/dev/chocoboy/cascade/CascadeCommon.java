7f0d18a159f6199ee45e0d5f51fe2b4d85fd949d
chocoboy <goombaniv1@gmail.com>
2026-06-26 11:13:59 +0200

    build(loader): add common and neoforge modules

---
+++ common/src/main/java/dev/chocoboy/cascade/CascadeCommon.java
@@ -0,0 +1,12 @@
+package dev.chocoboy.cascade;
+
+public final class CascadeCommon {
+
+    public static final String MOD_ID = "cascade";
+
+    private CascadeCommon() {
+    }
+
+    public static void init() {
+    }
+}
---
+++ neoforge/src/main/java/dev/chocoboy/cascade/neoforge/CascadeNeoForge.java
@@ -0,0 +1,12 @@
+package dev.chocoboy.cascade.neoforge;
+
+import dev.chocoboy.cascade.CascadeCommon;
+import net.neoforged.fml.common.Mod;
+
+@Mod(CascadeCommon.MOD_ID)
+public final class CascadeNeoForge {
+
+    public CascadeNeoForge() {
+        CascadeCommon.init();
+    }
+}
