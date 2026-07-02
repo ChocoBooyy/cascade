c373d759cfb49acb2e4f90152bbc1ac68d42fbcc
chocoboy <goombaniv1@gmail.com>
2026-07-02 19:02:23 +0200

    build(fabric): scaffold a fabric-loom module alongside moddev

---
+++ fabric/src/main/java/dev/chocoboy/cascade/fabric/CascadeFabric.java
@@ -0,0 +1,10 @@
+package dev.chocoboy.cascade.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class CascadeFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
